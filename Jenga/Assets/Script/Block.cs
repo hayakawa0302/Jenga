f6ef09c48a71ea0185a78b98a90cfff1405a7bdc
hayakawa0302 <melty_heaven@hotmail.com>
2015-07-01 22:19:46 +0900

    jenga20140701

---
+++ Jenga/Assets/Script/Block.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+	void Start () {
+		Color color;
+		int color_set = Random.Range (0, 3);
+		switch(color_set){
+		case 0:
+			color = Color.red;
+			GetComponent<Renderer>().material.color = color;
+			break;
+		case 1:
+			color = Color.green;
+			GetComponent<Renderer>().material.color = color;
+			break;
+		case 2:
+			color = Color.blue;
+			GetComponent<Renderer>().material.color = color;
+			break;
+		}
+	}
+
+	void Update () {
+
+	}
+}
---
+++ Jenga/Assets/Script/Start_ready.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start_ready : MonoBehaviour {
+	public float layer = 5; //積む段数
+	public float side = 3; //1段の本数
+	float drop_height = 2; //初期位置（高度）
+	public float blocksize = 0.5f; //ブロックのサイズ
+	public GameObject myCube;
+	
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < layer; i++){
+			for(int x = 0; x < side; x++){
+				if(i%2 == 0){
+					Instantiate (myCube, new Vector3 (blocksize, transform.position.y + i*blocksize + drop_height, transform.position.z + (x*blocksize)), Quaternion.Euler (0, i*90, 0));
+				}else{
+					Instantiate (myCube, new Vector3 (transform.position.x + (x*blocksize), transform.position.y + i*blocksize + drop_height, blocksize), Quaternion.Euler (0, i*90, 0));
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
